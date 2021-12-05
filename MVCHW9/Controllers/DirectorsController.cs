c83c4662e9f74404fa16b6327f2eeb76714bedde
morganseyler <morganseyler@ou.edu>
2021-12-05 12:11:48 -0600

    Add project files.

---
+++ MVCHW9/Controllers/DirectorsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCHW9.Models;
+
+namespace MVCHW9.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Directors
+        public ActionResult Index()
+        {
+            return View(db.Directors.ToList());
+        }
+
+        // GET: Directors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Director director = db.Directors.Find(id);
+            if (director == null)
+            {
+                return HttpNotFound();
+            }
+            return View(director);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
